9c089a95f14e74b5f5dede4f9b0061db60419360
Abdullahkhan90 <160678612+Abdullahkhan90@users.noreply.github.com>
2024-03-05 13:06:45 -0800

    Add files via upload

---
+++ 11.Names/Names.js
@@ -0,0 +1,5 @@
+"use strict";
+let names = ['Saif', 'Saad', 'Irshan', 'Hammad'];
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
---
+++ 11.Names/Names.ts
@@ -0,0 +1,9 @@
+
+
+let names :string[] = ['Saif','Saad','Irshan','Hammad'] ;
+
+for(let i=0; i<names.length; i++){
+
+    console.log(names[i]) ;
+
+}    
