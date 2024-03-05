ef95386e89f371ef3d02c9bd32c14650b724e730
Abdullahkhan90 <160678612+Abdullahkhan90@users.noreply.github.com>
2024-03-05 13:08:07 -0800

    Add files via upload

---
+++ 12.Greetings/Names.js
@@ -0,0 +1,6 @@
+"use strict";
+let names = ['Saif', 'Saad', 'Irshan', 'Hammad'];
+const message = 'Welcome to Typescript Class: ';
+for (let i = 0; i < names.length; i++) {
+    console.log(message + names[i]);
+}
---
+++ 12.Greetings/Names.ts
@@ -0,0 +1,11 @@
+
+
+let names :string[] = ['Saif','Saad','Irshan','Hammad'] ;
+
+const message :string = 'Welcome to Typescript Class: '
+
+for(let i=0; i<names.length; i++){
+
+    console.log(message + names[i]) ;
+
+}    
