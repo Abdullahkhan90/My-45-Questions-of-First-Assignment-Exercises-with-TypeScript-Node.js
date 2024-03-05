1257d9455da0f9c9dbb45675498c4fe1d60ff971
Abdullahkhan90 <160678612+Abdullahkhan90@users.noreply.github.com>
2024-03-05 13:21:22 -0800

    Add files via upload

---
+++ 20.Empty Array/Empty_Array.js
@@ -0,0 +1,12 @@
+"use strict";
+// Written by: Hafiz Abdullah
+// Date: 20-2-2024
+let countries = [];
+countries.push("England");
+countries.push("india");
+countries.push("Pakistan");
+// Printing the entire list.
+console.log(countries);
+countries.forEach(item => {
+    console.log("-", item);
+});
---
+++ 20.Empty Array/Empty_Array.ts
@@ -0,0 +1,18 @@
+// Written by: Hafiz Abdullah
+// Date: 20-2-2024
+
+let countries :string[] = [];
+
+countries.push("England");
+countries.push("india");
+countries.push("Pakistan");
+
+// Printing the entire list.
+
+console.log(countries);
+countries.forEach(item => {
+    console.log("-", item);
+
+});
+    
+
