f67c1bf565479e47a324b984aa21fece1e126bd6
Abdullahkhan90 <160678612+Abdullahkhan90@users.noreply.github.com>
2024-03-09 12:53:58 -0800

    Add files via upload

---
+++ 31. No Users/No_Users.js
@@ -0,0 +1,14 @@
+"use strict";
+//  Written by: Hafiz Abdullah.
+// Roll No: 00224647.
+// Array for usernames.
+let usernames = ['admin', 'Ali', 'Ahmed', 'Saad', 'Saif'];
+// Checking user Availibility
+if (usernames.length === 0) {
+    console.log("We need to find some users!");
+}
+else {
+    // Removing all usernames from the array.
+    usernames = [];
+    console.log("All usernames have been removed. " + usernames.length);
+}
