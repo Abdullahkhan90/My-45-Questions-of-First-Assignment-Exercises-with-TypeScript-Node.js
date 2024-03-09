2d0e819700933b6f87c58bf029bf9938d240a568
Abdullahkhan90 <160678612+Abdullahkhan90@users.noreply.github.com>
2024-03-09 13:06:49 -0800

    Add files via upload

---
+++ 41. Magicians/Magicians.js
@@ -0,0 +1,11 @@
+"use strict";
+// Written by : Hafiz Abdullah.
+// Assignment No: 41.
+// Array Magicians.
+const Magicians = ["Saad", "Michael", "David", "Hassan"];
+function show_magicians(Magicians) {
+    Magicians.forEach(Magicians => {
+        console.log(Magicians);
+    });
+}
+show_magicians(Magicians);
---
+++ 41. Magicians/Magicians.ts
@@ -0,0 +1,13 @@
+// Written by : Hafiz Abdullah.
+// Assignment No: 41.
+
+// Array Magicians.
+const Magicians: string[] = ["Saad", "Michael", "David", "Hassan"];
+
+function show_magicians(Magicians: string[]): void {
+    Magicians.forEach(Magicians => {
+    console.log(Magicians);
+    });
+}
+
+show_magicians(Magicians);
