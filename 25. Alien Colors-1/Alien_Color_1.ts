a3b90fe3d1d2b868bf6abcd28ab4fba895f67e59
Abdullahkhan90 <160678612+Abdullahkhan90@users.noreply.github.com>
2024-03-09 12:19:31 -0800

    Add files via upload

---
+++ 25. Alien Colors-1/Alien_Color_1.ts
@@ -0,0 +1,21 @@
+// written by: Hafiz Abdullah
+// Date: 21-2-2024
+
+
+// Alien's color define kia hai.
+let alien_color : string = 'green';
+
+// Green color check kiya hai.
+if (alien_color === 'green') {
+
+    console.log("Player just earned 5 points!");
+}
+
+// Alien's color change kardia ha.
+alien_color = 'yellow';
+
+// ye code nahi chalega kiu k condition False hai.
+if (alien_color === 'green') {
+
+    console.log("Player just earned 5 points!");
+}
