b4bb9e04d791e3055dda747e99ad7073f16ab08c
Abdullahkhan90 <160678612+Abdullahkhan90@users.noreply.github.com>
2024-03-09 12:16:03 -0800

    Add files via upload

---
+++ 23. Conditional Tests/Conditional_Tests.ts
@@ -0,0 +1,44 @@
+// Written by: Hafiz Abdullah
+// Date: 21-2-2024
+
+let car : string = 'subaru';
+
+// Printing Test 1.
+console.log("Is car == 'subaru'? I Predict True.");
+console.log(car == 'subaru');
+
+// Printing Test 2.
+console.log("Is car == 'honda'? I Predict False.");
+console.log(car == 'honda');
+
+// Printing Test 3.
+console.log("Is car != 'Ford'? I Predict True." );
+console.log(car != 'Ford');
+
+// Printing Test 4.
+console.log("Is car === 'subaru'? I Predict True.");
+console.log(car === 'subaru');
+
+// Printing Test 5.
+console.log("Is car !== 'subaru'? I Predict False.");
+console.log(car !== 'subaru');
+
+// Printing Test 6.
+console.log("Is car !== 'honda'? I Predict True.");
+console.log(car !== 'honda');
+
+// Printing Test 7.
+console.log("Is car == Cycle? I Predict False");
+console.log(car == "Cycle");
+
+// Printing Test 8.
+console.log("Is car === 5? I Predict False");
+console.log(car === "5");
+
+// Printing Test 9.
+console.log("Is car == 'Bus'? I Predict False");
+console.log(car == 'Bus');
+
+// Printing Test 10.
+console.log("Is car.toUpperCase() == 'SUBARU'? I Predict True.");
+console.log(car.toUpperCase() == 'SUBARU');
