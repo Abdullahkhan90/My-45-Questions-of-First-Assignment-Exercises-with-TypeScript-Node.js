8db0e4c483003db3c18af574744f57ec1b33f7a7
Abdullahkhan90 <160678612+Abdullahkhan90@users.noreply.github.com>
2024-03-09 13:04:18 -0800

    Add files via upload

---
+++ 39. City Names/City_Names.js
@@ -0,0 +1,11 @@
+"use strict";
+// Written By : Hafiz Abdullah.
+// Date: 21-2-2024.
+// Function for city_country.
+function city_country(city, country) {
+    return `${city},${country}`;
+}
+// Results.
+console.log(city_country("Lahore", "Pakistan"));
+console.log(city_country("New Delhi", "India"));
+console.log(city_country("Jeddah", "Saudia Arabia"));
---
+++ 39. City Names/City_Names.ts
@@ -0,0 +1,13 @@
+// Written By : Hafiz Abdullah.
+// Date: 21-2-2024.
+
+// Function for city_country.
+function city_country(city: string, country: string) : string {
+    return `${city},${country}`;
+}
+
+// Results.
+console.log(city_country("Lahore","Pakistan"));
+console.log(city_country("New Delhi","India"));
+console.log(city_country("Jeddah","Saudia Arabia"));
+
