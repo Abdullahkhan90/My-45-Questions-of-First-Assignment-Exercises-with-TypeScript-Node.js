0793cd6bc7e55da034792667101c4a84cfe9b18a
Abdullahkhan90 <160678612+Abdullahkhan90@users.noreply.github.com>
2024-03-09 12:51:06 -0800

    Add files via upload

---
+++ 29. Favourite Fruits/Favourite_Fruit.js
@@ -0,0 +1,18 @@
+"use strict";
+// written by: Hafiz Abdullah.
+const favourite_fruits = ["banana", "apple", "mango"];
+if (favourite_fruits.includes("banana")) {
+    console.log("I like bananas!");
+}
+if (favourite_fruits.includes("apple")) {
+    console.log("I like apples!");
+}
+if (favourite_fruits.includes("mango")) {
+    console.log("I like mangos!");
+}
+if (favourite_fruits.includes("orange")) {
+    console.log("I like oranges!");
+}
+if (favourite_fruits.includes("watermelon")) {
+    console.log("I like watermelons!");
+}
