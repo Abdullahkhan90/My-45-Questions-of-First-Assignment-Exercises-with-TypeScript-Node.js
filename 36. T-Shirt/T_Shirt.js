a2f4ab00e2587fd73c52ba2a73f27ef921414681
Abdullahkhan90 <160678612+Abdullahkhan90@users.noreply.github.com>
2024-03-09 13:01:01 -0800

    Add files via upload

---
+++ 36. T-Shirt/T_Shirt.js
@@ -0,0 +1,7 @@
+"use strict";
+// Written by: Hafiz Abdullah.
+// Date: 21-2-2024.
+function make_shirt(size, message) {
+    console.log(`Creating a ${size} shirt with the message: "${message}"`);
+}
+make_shirt('large', 'Nike!');
---
+++ 36. T-Shirt/T_Shirt.ts
@@ -0,0 +1,8 @@
+// Written by: Hafiz Abdullah.
+// Date: 21-2-2024.
+
+function make_shirt(size: string, message: string): void {
+    console.log(`Creating a ${size} shirt with the message: "${message}"`);
+}
+
+make_shirt('large', 'Nike!');
