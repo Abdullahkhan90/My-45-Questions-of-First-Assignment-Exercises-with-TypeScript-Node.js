6ddddff4b51365959066254d520a15b876d7b689
Abdullahkhan90 <160678612+Abdullahkhan90@users.noreply.github.com>
2024-03-09 12:52:17 -0800

    Add files via upload

---
+++ 30. Hello Admin/Hello_Admin.js
@@ -0,0 +1,15 @@
+"use strict";
+// Written by: Hafiz Abdullah.
+//  Roll No: 00224647
+// Array for Usernames.
+const Usernames = ['admin', 'Ali', 'Ahmed', 'Saad', 'Saif'];
+// Using foreach method
+Usernames.forEach(username => {
+    // Step 3: Inside the loop, special greeting for 'admin' , For Others printing a simple greeting.
+    if (username.toLowerCase() === 'admin') {
+        console.log('Hello admin, would you like to see a status report?');
+    }
+    else {
+        console.log(`Hello  {usernames}, thank you for logging in again.`);
+    }
+});
