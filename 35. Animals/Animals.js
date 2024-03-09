9e6c96915765f13699bd32b542aec8d6256e20c0
Abdullahkhan90 <160678612+Abdullahkhan90@users.noreply.github.com>
2024-03-09 12:59:55 -0800

    Add files via upload

---
+++ 35. Animals/Animals.js
@@ -0,0 +1,11 @@
+"use strict";
+// Written by: Hafiz Abdullah.
+// Date: 21-2-2024.
+// Animals.
+const animals = ["cat", "dog", "parrot"];
+// Statements about each animal.
+for (let animal of animals) {
+    console.log(`A ${animal} would make a great pet.`);
+}
+// Results.
+console.log("Any of these animals would make a great pet!");
---
+++ 35. Animals/Animals.ts
@@ -0,0 +1,14 @@
+// Written by: Hafiz Abdullah.
+// Date: 21-2-2024.
+
+// Animals.
+const animals: string[] = ["cat", "dog", "parrot"];
+
+// Statements about each animal.
+for (let animal of animals) {
+    console.log(`A ${animal} would make a great pet.`);
+}
+
+// Results.
+console.log("Any of these animals would make a great pet!");
+
