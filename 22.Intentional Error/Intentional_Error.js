67ea98e24a929d2f4f85ebcac7fa1d367de68d49
Abdullahkhan90 <160678612+Abdullahkhan90@users.noreply.github.com>
2024-03-09 12:14:37 -0800

    Add files via upload

---
+++ 22.Intentional Error/Intentional_Error.js
@@ -0,0 +1,9 @@
+"use strict";
+// Written by: Hafiz Abdullah
+// Date: 21-2-2024
+// Creating an array.
+let myArray = [1, 2, 3, 4, 5];
+// Accessing array by index.
+console.log(myArray[10]);
+// Correct the error.
+console.log(myArray[4]);
---
+++ 22.Intentional Error/Intentional_Error.ts
@@ -0,0 +1,11 @@
+// Written by: Hafiz Abdullah
+// Date: 21-2-2024
+
+// Creating an array.
+let myArray: number[] = [1, 2, 3, 4, 5];
+
+// Accessing array by index.
+console.log(myArray[10]);
+
+// Correct the error.
+console.log(myArray[4]);
